d9f810b3971decd07ab78a7656bebb2d97cb4182
Artem Mikhaylov <m.av1922@yandex.ru>
2023-07-01 10:10:09 +0300

    Initial Commit

---
+++ Gradient and shadow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gradient and shadow
+//
+//  Created by Артем Михайлов on 01.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
